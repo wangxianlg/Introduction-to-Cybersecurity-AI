40cc05b41657ecc43382c27c7d613217cd0add21
wangxianlg <34946467+wangxianlg@users.noreply.github.com>
2025-10-26 14:47:46 -0400

    Create ai-data-analysis-10262025.py

---
+++ ai-data-analysis-10262025.py
@@ -0,0 +1,16 @@
+from pandasai import SmartDataframe
+import pandas as pd
+from langchain_community.llms import Ollama
+
+df = pd.DataFrame({
+    "country": [
+        "United States", "United Kingdom", "France", "Germany", "Italy", "Spain", "Canada", "Australia", "Japan", "China"],
+    "gdp": [
+        19294482071552, 2891615567872, 2411255037952, 3435817336832, 1745433788416, 1181205135360, 1607402389504, 1490967855104, 4380756541440, 14631844184064
+    ],
+})
+
+llm = Ollama(base_url='http://192.168.1.144:11434', model="llama3.2:latest")
+
+df = SmartDataframe(df, config={"llm": llm})
+df.chat("can you make a bar graph of all the countries' GDP")
